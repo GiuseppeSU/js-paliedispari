19c5c262a4734f5ac8a37ded9c2d1979bc38e548
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-03-02 15:33:31 +0100

    Fine prima funzione

---
+++ pari e dispari/js/main.js
@@ -0,0 +1,10 @@
+let scegli = prompt('scegli pari o dispari');
+
+function numberRandom(min, max) {
+    return Math.random() * (max - min) + min;
+    
+  }
+
+  console.log(numberRandom(1,5));
+
+  function pariDispari ()
